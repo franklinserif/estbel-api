181577c57be4d5e27aef276e5e017536bb21e230
franklin rodriguez <franklinserif@gmail.com>
2024-12-24 11:03:13 -0400

    feat(create-user.dto.ts): add properties

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength, IsPhoneNumber } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  firstName: string;
+
+  @IsString()
+  @MinLength(2)
+  lastName: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsString()
+  password: string;
+}
