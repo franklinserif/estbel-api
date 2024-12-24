5bd3f9a12b06235ad6d15f587f5581637a20b016
franklin rodriguez <franklinserif@gmail.com>
2024-12-24 10:01:53 -0400

    refactor(modules.module.ts): add TypeOrmModule to load entities

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ModulesService } from './modules.service';
 import { ModulesController } from './modules.controller';
+import { Module as ModuleEntity } from './entities/module.entity';
+import { UserModuleAccess } from './entities/user-module-access.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ModulesController],
+  imports: [TypeOrmModule.forFeature([ModuleEntity, UserModuleAccess])],
   providers: [ModulesService],
+  exports: [TypeOrmModule],
 })
 export class ModulesModule {}
