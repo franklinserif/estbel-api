26284a34d1d76733f8198011a7973aef01e3a5c9
franklin rodriguez <franklinserif@gmail.com>
2024-12-24 10:01:16 -0400

    feat(entities): add user module access entities

---
+++ src/modules/entities/user-module-access.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '@users/entities/user.entity';
+import { Module } from './module.entity';
+
+@Entity('user_module_access')
+export class UserModuleAccess {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('bool', {
+    default: false,
+  })
+  canRead: boolean;
+
+  @Column('bool', {
+    default: false,
+  })
+  canEdit: boolean;
+
+  @Column('bool', {
+    default: false,
+  })
+  canDelete: boolean;
+
+  @ManyToOne(() => User, (user) => user.userModuleAccess)
+  user: User;
+
+  @ManyToOne(() => Module, (module) => module.userModuleAccess)
+  module: Module;
+}
