dcf474297de6c133f5e3f2a49d73aaedf293b530
franklin rodriguez <franklinserif@gmail.com>
2024-12-24 10:00:46 -0400

    feat(entities): add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { UserModuleAccess } from '@modules/entities/user-module-access.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  firstName: string;
+
+  @Column('text', {})
+  lastName: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('text')
+  phone: string;
+
+  @Column('text', {
+    select: false,
+  })
+  password: string;
+
+  @OneToMany(
+    () => UserModuleAccess,
+    (userModuleAccess) => userModuleAccess.user,
+  )
+  userModuleAccess: UserModuleAccess[];
+}
