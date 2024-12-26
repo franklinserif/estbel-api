60172e4d65c8a83660663e44bb6da07cd96b8606
franklin rodriguez <franklinserif@gmail.com>
2024-12-26 14:27:14 -0400

    style(users.service): change imports code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
