41449f6daa62e087922ecc478590c1cfca2dd678
franklin rodriguez <franklinserif@gmail.com>
2024-12-26 16:35:58 -0400

    refactor(seeds.service.ts): improve logic

--- src/seeds/seeds.service.ts
+++ src/seeds/seeds.service.ts
@@ -2,6 +2,10 @@ import { DataSource } from 'typeorm';
 import { Injectable, Logger } from '@nestjs/common';
 import { User } from '@users/entities/user.entity';
 import { users } from './seed/users';
+import { Module } from '@modules/entities/module.entity';
+import { modules } from './seed/modules';
+import { user_module_access } from './seed/user_module_access';
+import { UserModuleAccess } from '@modules/entities/user-module-access.entity';
 
 @Injectable()
 export class SeedsService {
@@ -13,6 +17,8 @@ export class SeedsService {
     const queryRunner = this.dataSource.createQueryRunner();
 
     await queryRunner.query('DELETE FROM "users"');
+    await queryRunner.query('DELETE FROM "modules"');
+    await queryRunner.query('DELETE FROM "user_module_access"');
 
     try {
       await queryRunner.connect();
@@ -20,11 +26,36 @@ export class SeedsService {
 
       await queryRunner.query('DELETE FROM "users"');
 
+      const modulesEntities = modules.map((moduleEntity) =>
+        queryRunner.manager.create(Module, moduleEntity),
+      );
+
+      const modulesResults = await queryRunner.manager.save(modulesEntities);
+
       const usersEntities = users.map((userEntity) =>
         queryRunner.manager.create(User, userEntity),
       );
 
-      await queryRunner.manager.save(usersEntities);
+      const usersModify = usersEntities.map((user) => {
+        const userModuleAccess = user.userModuleAccess.map(
+          (userModuleAccess) => {
+            const userModuleAccessIndex = Math.floor(Math.random() * 8) + 0;
+            const moduleIndex = Math.floor(Math.random() * 2) + 0;
+
+            const UMA = {
+              ...user_module_access[userModuleAccessIndex],
+              module: modulesResults[moduleIndex],
+            };
+
+            const result = queryRunner.manager.create(UserModuleAccess, UMA);
+            return result;
+          },
+        );
+        user.userModuleAccess = userModuleAccess;
+
+        return user;
+      });
+      await queryRunner.manager.save(usersModify);
 
       await queryRunner.commitTransaction();
       this.logger.log('Seeds executed succesfully');
