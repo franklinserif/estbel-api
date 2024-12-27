bc32941406cd99cac57454b07d1480821db0dada
franklin rodriguez <franklinserif@gmail.com>
2024-12-27 11:51:47 -0400

    feat(modules.service): implement logic

--- src/modules/modules.service.ts
+++ src/modules/modules.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Module } from './entities/module.entity';
+import { CreateModuleDto } from './dto/create-module.dto';
+import { UpdateModuleDto } from './dto/update-module.dto';
 
 @Injectable()
-export class ModulesService {}
+export class ModulesService {
+  constructor(
+    @InjectRepository(Module)
+    private readonly moduleRepository: Repository<Module>,
+  ) {}
+
+  async create(createModuleDto: CreateModuleDto) {
+    const module = this.moduleRepository.create(createModuleDto);
+
+    return await this.moduleRepository.save(module);
+  }
+
+  async findAll() {
+    const module = await this.moduleRepository.find();
+
+    return module;
+  }
+
+  async findOne(id: string) {
+    const module = await this.moduleRepository.findOne({ where: { id } });
+
+    if (!module?.id) {
+      throw new NotFoundException(`Module with id: ${id} not found`);
+    }
+
+    return module;
+  }
+
+  async update(id: string, updateModuleDto: UpdateModuleDto) {
+    await this.findOne(id);
+
+    const updatedModule = await this.moduleRepository.update(
+      id,
+      updateModuleDto,
+    );
+
+    return updatedModule;
+  }
+
+  async remove(id: string) {
+    await this.findOne(id);
+
+    return await this.moduleRepository.delete(id);
+  }
+}
