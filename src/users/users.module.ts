3e15283e03349da0f53d9f882d86c90f2bf3d47d
franklin rodriguez <franklinserif@gmail.com>
2024-12-27 13:19:07 -0400

    feat(users.module): imports UserModuleAccessModule, ModulesModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModuleAccessModule } from '@user-module-access/user-module-access.module';
+import { ModulesModule } from '@modules/modules.module';
 
 @Module({
   controllers: [UsersController],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UserModuleAccessModule,
+    ModulesModule,
+  ],
   providers: [UsersService],
   exports: [TypeOrmModule],
 })
