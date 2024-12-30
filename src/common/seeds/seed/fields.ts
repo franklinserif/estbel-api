c3526f0338d012701cf86dbbf9cfdc0d2470a721
franklin rodriguez <franklinserif@gmail.com>
2024-12-30 17:15:30 -0400

    feat(seeds): seed

---
+++ src/common/seeds/seed/fields.ts
@@ -0,0 +1,38 @@
+export const fields = [
+  {
+    fieldName: 'firstName',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'lastName',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'age',
+    fieldType: 'number',
+  },
+  {
+    fieldName: 'email',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'country',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'city',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'phone',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'profession',
+    fieldType: 'text',
+  },
+  {
+    fieldName: 'gender',
+    fieldType: 'text',
+  },
+];
---
+++ src/common/seeds/seed/members.ts
@@ -0,0 +1,368 @@
+export const members = [
+  {
+    ci: '123456789',
+    fields: [
+      {
+        fieldValue: 'John',
+        field: {
+          fieldName: 'firstName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Doe',
+        field: {
+          fieldName: 'lastName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '25',
+        field: {
+          fieldName: 'age',
+          fieldType: 'number',
+        },
+      },
+      {
+        fieldValue: 'john.doe@example.com',
+        field: {
+          fieldName: 'email',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'USA',
+        field: {
+          fieldName: 'country',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'New York',
+        field: {
+          fieldName: 'city',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '+11234567890',
+        field: {
+          fieldName: 'phone',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Software Engineer',
+        field: {
+          fieldName: 'profession',
+          fieldType: 'text',
+        },
+      },
+    ],
+  },
+  {
+    ci: '987654321',
+    fields: [
+      {
+        fieldValue: 'Jane',
+        field: {
+          fieldName: 'firstName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Smith',
+        field: {
+          fieldName: 'lastName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '30',
+        field: {
+          fieldName: 'age',
+          fieldType: 'number',
+        },
+      },
+      {
+        fieldValue: 'jane.smith@example.com',
+        field: {
+          fieldName: 'email',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Canada',
+        field: {
+          fieldName: 'country',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Toronto',
+        field: {
+          fieldName: 'city',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '+14165551234',
+        field: {
+          fieldName: 'phone',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Doctor',
+        field: {
+          fieldName: 'profession',
+          fieldType: 'text',
+        },
+      },
+    ],
+  },
+  {
+    ci: '456789123',
+    fields: [
+      {
+        fieldValue: 'Michael',
+        field: {
+          fieldName: 'firstName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Brown',
+        field: {
+          fieldName: 'lastName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '40',
+        field: {
+          fieldName: 'age',
+          fieldType: 'number',
+        },
+      },
+      {
+        fieldValue: 'michael.brown@example.com',
+        field: {
+          fieldName: 'email',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'UK',
+        field: {
+          fieldName: 'country',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'London',
+        field: {
+          fieldName: 'city',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '+447911123456',
+        field: {
+          fieldName: 'Phone',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Teacher',
+        field: {
+          fieldName: 'profession',
+          fieldType: 'text',
+        },
+      },
+    ],
+  },
+  {
+    ci: '159753486',
+    fields: [
+      {
+        fieldValue: 'Alice',
+        field: {
+          fieldName: 'firstName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Johnson',
+        field: {
+          fieldName: 'lastName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '28',
+        field: {
+          fieldName: 'age',
+          fieldType: 'number',
+        },
+      },
+      {
+        fieldValue: 'alice.johnson@example.com',
+        field: {
+          fieldName: 'email',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Australia',
+        field: {
+          fieldName: 'country',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Sydney',
+        field: {
+          fieldName: 'city',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '+61234567890',
+        field: {
+          fieldName: 'phone',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Architect',
+        field: {
+          fieldName: 'profession',
+          fieldType: 'text',
+        },
+      },
+    ],
+  },
+  {
+    ci: '753951852',
+    fields: [
+      {
+        fieldValue: 'Emma',
+        field: {
+          fieldName: 'firstName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Davis',
+        field: {
+          fieldName: 'lastName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '35',
+        field: {
+          fieldName: 'age',
+          fieldType: 'number',
+        },
+      },
+      {
+        fieldValue: 'emma.davis@example.com',
+        field: {
+          fieldName: 'email',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'New Zealand',
+        field: {
+          fieldName: 'country',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Auckland',
+        field: {
+          fieldName: 'city',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '+6491234567',
+        field: {
+          fieldName: 'phone',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Pilot',
+        field: {
+          fieldName: 'profession',
+          fieldType: 'text',
+        },
+      },
+    ],
+  },
+  {
+    ci: '852963741',
+    fields: [
+      {
+        fieldValue: 'Oliver',
+        field: {
+          fieldName: 'firstName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Wilson',
+        field: {
+          fieldName: 'lastName',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '45',
+        field: {
+          fieldName: 'age',
+          fieldType: 'number',
+        },
+      },
+      {
+        fieldValue: 'oliver.wilson@example.com',
+        field: {
+          fieldName: 'email',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Germany',
+        field: {
+          fieldName: 'country',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Berlin',
+        field: {
+          fieldName: 'city',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: '+4915123456789',
+        field: {
+          fieldName: 'phone',
+          fieldType: 'text',
+        },
+      },
+      {
+        fieldValue: 'Chef',
+        field: {
+          fieldName: 'profession',
+          fieldType: 'text',
+        },
+      },
+    ],
+  },
+];
