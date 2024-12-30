3b397659cb7097def2013bb7a6e209dc044d0274
franklin rodriguez <franklinserif@gmail.com>
2024-12-30 17:16:36 -0400

    refactor(members module): export TypeOrmModule

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -3,10 +3,11 @@ import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
+import { FieldsModule } from '@fields/fields.module';
 
 @Module({
   controllers: [MembersController],
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member]), FieldsModule],
   providers: [MembersService],
   exports: [TypeOrmModule],
 })
