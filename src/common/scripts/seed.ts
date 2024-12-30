78819f9d1172b53c00929be7d46705e7931320b5
franklin rodriguez <franklinserif@gmail.com>
2024-12-30 14:42:47 -0400

    chore(seed): change seed to common folder

--- src/common/scripts/seed.ts
+++ src/common/scripts/seed.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { SeedsModule } from '@seeds/seeds.module';
-import { SeedsService } from '@seeds/seeds.service';
+import { SeedsModule } from '@common/seeds/seeds.module';
+import { SeedsService } from '@common/seeds/seeds.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(SeedsModule);
