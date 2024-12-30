225a087876b22769aadde814e9b96f03d5e28b70
franklin rodriguez <franklinserif@gmail.com>
2024-12-30 11:50:12 -0400

    refactor(scripts): move script folder inside common

--- src/common/scripts/seed.ts
+++ src/common/scripts/seed.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { SeedsModule } from '../seeds/seeds.module';
-import { SeedsService } from '../seeds/seeds.service';
+import { SeedsModule } from '@seeds/seeds.module';
+import { SeedsService } from '@seeds/seeds.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(SeedsModule);
