c53422ee0b74d4668e5f301961ada7b276b92241
franklin rodriguez <franklinserif@gmail.com>
2024-12-31 17:15:27 -0400

    chore(events.module): import ScheduleModule

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -5,10 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { Attendance } from './entities/attendance.entity';
 import { MembersModule } from '@members/members.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   controllers: [EventsController],
-  imports: [TypeOrmModule.forFeature([Event, Attendance]), MembersModule],
+  imports: [
+    TypeOrmModule.forFeature([Event, Attendance]),
+    ScheduleModule.forRoot(),
+    MembersModule,
+  ],
   providers: [EventsService],
   exports: [TypeOrmModule],
 })
