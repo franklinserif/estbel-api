c1ece4470827b47bafd90c105333126455a6354f
franklin rodriguez <franklinserif@gmail.com>
2025-01-04 14:37:10 -0400

    refactor(schedule.service): improve cron jobs implementation

--- src/events/schedule.service.ts
+++ src/events/schedule.service.ts
@@ -2,94 +2,141 @@ import { Injectable, Logger } from '@nestjs/common';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
 import { Event } from './entities/event.entity';
+import { JobInfo } from '@common/interfaces/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TIME_ZONE_CARACAS } from '@common/constants/timeZone';
 
 @Injectable()
 export class ScheduleService {
   private readonly logger = new Logger(ScheduleService.name);
-  constructor(private schedulerRegistry: SchedulerRegistry) {}
+  constructor(
+    private schedulerRegistry: SchedulerRegistry,
+    @InjectRepository(Event)
+    private readonly eventRepository: Repository<Event>,
+  ) {}
 
   findAll() {
     const cronJobs = this.schedulerRegistry.getCronJobs();
 
-    console.log('con-jobs:  ', cronJobs);
+    const jobs = Array.from(cronJobs.entries()).map(([name, job]) => ({
+      name,
+      nextInvocation: job.nextDate().toString(),
+    }));
 
-    return cronJobs;
+    return jobs;
   }
 
   addCronJobs(events: Event[]) {
     for (const event of events) {
-      const cronJob = new CronJob(event.cronExpression, () => {
+      const startCronJob = new CronJob(event.startCronExpression, async () => {
         this.logger.log(`The repeating event ${event.id} has started.`);
         this.notifyUsers(event.id);
       });
 
       this.schedulerRegistry.addCronJob(
-        `event-${event.id}-${event.repeat ? 'weekly' : 'day'}`,
-        cronJob,
+        `event-${event.id}-start-${event.repeat ? 'weekly' : 'day'}`,
+        startCronJob,
       );
 
-      cronJob.start();
+      startCronJob.start();
+
+      const endCronJob = new CronJob(event.endCronExpression, async () => {
+        this.logger.log(`The repeating event ${event.id} has started.`);
+        this.notifyUsers(event.id);
+      });
+
+      this.schedulerRegistry.addCronJob(
+        `event-${event.id}-end-${event.repeat ? 'weekly' : 'day'}`,
+        endCronJob,
+      );
+
+      endCronJob.start();
     }
   }
 
-  scheduleEventNotification(event: Event) {
-    const cronExpression = this.generateWeeklyCronExpression(
+  scheduleEventNotification(event: Event): JobInfo {
+    const startCronExpression = this.generateWeeklyCronExpression(
       event.startTime,
       event.repeat,
     );
-    const job = new CronJob(cronExpression, () => {
-      this.logger.log(`The repeating event ${event.id} has started.`);
-      this.notifyUsers(event.id);
-    });
 
-    const jobKey = `event-${event.id}-${event.repeat ? 'weekly' : 'day'}`;
+    const startJob = this.job(
+      startCronExpression,
+      `The repeating event ${event.id} has started.`,
+      event.id,
+      true,
+    );
 
-    this.schedulerRegistry.addCronJob(jobKey, job);
+    const startJobKey = `event-${event.id}-start-${event.repeat ? 'weekly' : 'day'}`;
 
-    job.start();
+    this.schedulerRegistry.addCronJob(startJobKey, startJob);
 
-    return { cronExpression, jobKey };
+    startJob.start();
+    const endCronExpression = this.generateWeeklyCronExpression(
+      event.endTime,
+      event.repeat,
+    );
+
+    const endJob = this.job(
+      endCronExpression,
+      `The repeating event ${event.id} has ended.`,
+      event.id,
+      false,
+    );
+
+    const endJobKey = `event-${event.id}-end-${event.repeat ? 'weekly' : 'day'}`;
+
+    this.schedulerRegistry.addCronJob(endJobKey, endJob);
+
+    endJob.start();
+
+    return {
+      start: { cronExpression: startCronExpression, jobKey: startJobKey },
+      end: { cronExpression: endCronExpression, jobKey: endJobKey },
+    };
   }
 
   private generateWeeklyCronExpression(startTime: Date, repeat: boolean) {
-    const minutes = startTime.getUTCMinutes();
-    const hours = startTime.getUTCHours();
-    const dayOfMonth = startTime.getUTCDate();
-    const month = startTime.getUTCMonth() + 1;
-    const dayOfWeek = startTime.getUTCDay();
+    const seconds = startTime.getSeconds();
+    const minutes = startTime.getMinutes();
+    const hours = startTime.getHours();
+    const dayOfWeek = startTime.getDay();
 
     if (repeat) {
-      return `${minutes} ${hours} * * ${dayOfWeek}`;
+      return `${seconds} ${minutes} ${hours} * * ${dayOfWeek}`;
     } else {
-      return `${minutes} ${hours} ${dayOfMonth} ${month} *`;
+      return `${seconds} ${minutes} ${hours} ${startTime.getDate()} ${startTime.getMonth() + 1} *`;
     }
   }
 
   updateCronJob(oldEvent: Event, newEvent: Event) {
-    const jobKey = `event-${oldEvent.id}-${oldEvent.repeat ? 'weekly' : 'day'}`;
+    const startJobKey = `event-${oldEvent.id}-start-${oldEvent.repeat ? 'weekly' : 'day'}`;
 
-    const existingJob = this.schedulerRegistry.getCronJob(jobKey);
+    const existingStartJob = this.schedulerRegistry.getCronJob(startJobKey);
 
-    if (existingJob) {
-      existingJob.stop();
-      this.schedulerRegistry.deleteCronJob(jobKey);
+    if (existingStartJob) {
+      existingStartJob.stop();
+      this.schedulerRegistry.deleteCronJob(startJobKey);
       this.logger.log(`Cron job for event ${newEvent.id} removed.`);
     }
 
-    const newCronExpression = this.generateWeeklyCronExpression(
-      newEvent.startTime,
-      newEvent.repeat,
-    );
-    const newJob = new CronJob(newCronExpression, () => {
-      this.logger.log(
-        `The updated repeating event ${newEvent.id} has started.`,
-      );
-      this.notifyUsers(newEvent.id);
-    });
+    const endJobKey = `event-${oldEvent.id}-end-${oldEvent.repeat ? 'weekly' : 'day'}`;
 
-    this.schedulerRegistry.addCronJob(jobKey, newJob);
-    newJob.start();
-    this.logger.log(`Cron job for event ${newEvent.id} updated with new time.`);
+    const existingEndJob = this.schedulerRegistry.getCronJob(endJobKey);
+
+    if (existingEndJob) {
+      existingEndJob.stop();
+      this.schedulerRegistry.deleteCronJob(endJobKey);
+      this.logger.log(`Cron job for event ${newEvent.id} removed.`);
+    }
+
+    this.scheduleEventNotification({
+      ...oldEvent,
+      ...newEvent,
+      startTime: new Date(newEvent.startTime),
+      endTime: new Date(newEvent.endTime),
+    } as Event);
   }
 
   cancelEvent(event: Event): void {
@@ -101,5 +148,28 @@ export class ScheduleService {
 
   private notifyUsers(eventId: string) {
     this.logger.log('notify users..');
+  }
+
+  private job(
+    cronExpression: string,
+    message: string,
+    eventId: string,
+    isActive: boolean,
+  ): CronJob<any, null> {
+    const endJob = new CronJob(
+      cronExpression,
+      async () => {
+        this.logger.log(message);
+        this.notifyUsers(eventId);
+        await this.eventRepository.update(eventId, {
+          isActive,
+        });
+      },
+      null,
+      false,
+      TIME_ZONE_CARACAS,
+    );
+
+    return endJob;
   }
 }
