38b86f92f05734f2c979cdc694144163c9274f09
franklin rodriguez <franklinserif@gmail.com>
2025-01-04 20:05:24 -0400

    feat(reports): add fonts

---
+++ src/reports/documents/styles/fonts.ts
@@ -0,0 +1,8 @@
+export const fonts = {
+  Roboto: {
+    normal: 'public/fonts/Roboto/Roboto-Regular.ttf',
+    bold: 'public/fonts/Roboto/Roboto-Medium.ttf',
+    italics: 'public/fonts/Roboto/Roboto-Italic.ttf',
+    bolditalics: 'public/fonts/Roboto/Roboto-MediumItalic.ttf',
+  },
+};
