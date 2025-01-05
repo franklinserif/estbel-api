b27ab27bc765956acbea0bdc0de2832ce04f323c
franklin rodriguez <franklinserif@gmail.com>
2025-01-04 20:07:32 -0400

    feat(reports.module): add PrinterService

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { PrinterService } from './printer.service';
 
 @Module({
   controllers: [ReportsController],
-  providers: [ReportsService],
+  providers: [ReportsService, PrinterService],
 })
 export class ReportsModule {}
