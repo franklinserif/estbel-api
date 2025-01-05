7e6fddde0e16b9d3939161f1a533331bae62173a
franklin rodriguez <franklinserif@gmail.com>
2025-01-04 20:06:42 -0400

    feat(report.service): implement test report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateReportDto } from './dto/create-report.dto';
-import { UpdateReportDto } from './dto/update-report.dto';
+import { PrinterService } from './printer.service';
+import { testReport } from './documents/test.report';
 
 @Injectable()
 export class ReportsService {
-  create(createReportDto: CreateReportDto) {
-    return 'This action adds a new report';
-  }
+  constructor(private readonly printerService: PrinterService) {}
 
-  findAll() {
-    return `This action returns all reports`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} report`;
-  }
-
-  update(id: number, updateReportDto: UpdateReportDto) {
-    return `This action updates a #${id} report`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} report`;
+  async test(): Promise<PDFKit.PDFDocument> {
+    return this.printerService.createPDF(testReport());
   }
 }
