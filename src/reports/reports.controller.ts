b76f30a8c0787a8dfeb2433f5fb2319d822605e7
franklin rodriguez <franklinserif@gmail.com>
2025-01-04 20:06:57 -0400

    feat(reports.controller): implement test endpoint

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ReportsService } from './reports.service';
-import { CreateReportDto } from './dto/create-report.dto';
-import { UpdateReportDto } from './dto/update-report.dto';
+import { Response } from 'express';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
-  @Post()
-  create(@Body() createReportDto: CreateReportDto) {
-    return this.reportsService.create(createReportDto);
-  }
+  @Get('test')
+  async test(@Res() response: Response) {
+    const pdfDoc = await this.reportsService.test();
 
-  @Get()
-  findAll() {
-    return this.reportsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reportsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReportDto: UpdateReportDto) {
-    return this.reportsService.update(+id, updateReportDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reportsService.remove(+id);
+    response.setHeader('Content-type', 'application/pdf');
+    pdfDoc.info.Title = 'test';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
   }
 }
