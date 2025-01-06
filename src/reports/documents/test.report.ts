0464cc864643ca934cb87f4d053806b2eb924a86
franklin rodriguez <franklinserif@gmail.com>
2025-01-06 16:28:18 -0400

    feat(test.report): add test report

--- src/reports/documents/test.report.ts
+++ src/reports/documents/test.report.ts
@@ -1,12 +1,51 @@
-import type { TDocumentDefinitions } from 'pdfmake/interfaces';
+import type { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { getFormatterDate } from '@common/libs/date';
+
+const styles: StyleDictionary = {
+  header: { margin: [40, 20], fontSize: 14, alignment: 'right' },
+  table: { margin: [10, 20] },
+};
 
 export const testReport = (): TDocumentDefinitions => {
   return {
     header: {
-      text: 'Bill report',
-      alignment: 'right',
-      margin: [10, 10],
+      text: getFormatterDate(),
+
+      style: 'header',
     },
-    content: ['Hola mundo', 'Franklin Rodriguez'],
+    content: [
+      {
+        canvas: [
+          {
+            type: 'line',
+            x1: 2,
+            y1: 5,
+            x2: 520,
+            y2: 5,
+            lineWidth: 1,
+          },
+        ],
+      },
+
+      {
+        style: 'table',
+        layout: {
+          fillColor: function (rowIndex, node, columnIndex) {
+            return rowIndex % 2 === 0 && rowIndex !== 0 ? '#ebebeb' : null;
+          },
+        },
+        table: {
+          body: [
+            ['Column 1', 'Column 2', 'Column 3', 'Column 4'],
+            ['One value goes here', 'Another one here', 'OK?', 'Test'],
+            ['One value goes here', 'Another one here', 'OK?', 'Test'],
+            ['One value goes here', 'Another one here', 'OK?', 'Test'],
+            ['One value goes here', 'Another one here', 'OK?', 'Test'],
+            ['One value goes here', 'Another one here', 'OK?', 'Test'],
+          ],
+        },
+      },
+    ],
+    styles: styles,
   };
 };
