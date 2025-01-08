c163693c46f89a37b663fb33d48400075dfe0a03
franklin rodriguez <franklinserif@gmail.com>
2025-01-08 16:26:43 -0400

    feat(documents): add members and users reports

---
+++ src/reports/documents/members.report.ts
@@ -0,0 +1,65 @@
+import type { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { getFormatterDate } from '@common/libs/date';
+import { Member } from '@members/entities/member.entity';
+import { Field } from '@fields/entities/field.entity';
+
+const styles: StyleDictionary = {
+  header: { margin: [40, 20], fontSize: 14, alignment: 'right' },
+  table: { margin: [0, 10, 0, 0], alignment: 'center' },
+};
+
+const tableHead = [
+  { text: 'N°', margin: [5, 5] },
+  { text: 'Nombre', margin: [5, 5] },
+  { text: 'Apellido', margin: [5, 5] },
+  { text: 'Email', margin: [5, 5] },
+  { text: 'Teléfono', margin: [5, 5] },
+];
+
+export const membersReport = (
+  members: Member[],
+  fields: Field[],
+): TDocumentDefinitions => {
+  return {
+    pageOrientation: 'landscape',
+    pageSize: 'LETTER',
+    pageMargins: [20, 50, 20, 20],
+    header: (currentPage, pageCount) => {
+      return {
+        style: 'header',
+        columns: [
+          {
+            text: `Página ${currentPage} de ${pageCount}`,
+            alignment: 'left',
+          },
+          {
+            text: getFormatterDate(),
+            alignment: 'right',
+          },
+        ],
+      };
+    },
+    content: [
+      {
+        style: 'table',
+        layout: {
+          fillColor: function (rowIndex, node, columnIndex) {
+            return rowIndex % 2 === 0 && rowIndex !== 0 ? '#ebebeb' : null;
+          },
+        },
+        table: {
+          dontBreakRows: true,
+
+          headerRows: 1,
+          body: [
+            [...fields.map((field) => field.fieldName)],
+            ...members.map((member, index) => [
+              member?.fields.map((field) => field?.fieldValue),
+            ]),
+          ],
+        },
+      },
+    ],
+    styles: styles,
+  };
+};
---
+++ src/reports/documents/users.report.ts
@@ -0,0 +1,65 @@
+import type { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { getFormatterDate } from '@common/libs/date';
+import { User } from '@users/entities/user.entity';
+
+const styles: StyleDictionary = {
+  header: { margin: [40, 20], fontSize: 14, alignment: 'right' },
+  table: { margin: [0, 10, 0, 0], alignment: 'center' },
+};
+
+const tableHead = [
+  { text: 'N°', margin: [5, 5] },
+  { text: 'Nombre', margin: [5, 5] },
+  { text: 'Apellido', margin: [5, 5] },
+  { text: 'Email', margin: [5, 5] },
+  { text: 'Teléfono', margin: [5, 5] },
+];
+
+export const userReport = (users: User[]): TDocumentDefinitions => {
+  return {
+    pageOrientation: 'landscape',
+    pageSize: 'LETTER',
+    pageMargins: [20, 50, 20, 20],
+    header: (currentPage, pageCount) => {
+      return {
+        style: 'header',
+        columns: [
+          {
+            text: `Página ${currentPage} de ${pageCount}`,
+            alignment: 'left',
+          },
+          {
+            text: getFormatterDate(),
+            alignment: 'right',
+          },
+        ],
+      };
+    },
+    content: [
+      {
+        style: 'table',
+        layout: {
+          fillColor: function (rowIndex, node, columnIndex) {
+            return rowIndex % 2 === 0 && rowIndex !== 0 ? '#ebebeb' : null;
+          },
+        },
+        table: {
+          dontBreakRows: true,
+          widths: [30, 'auto', 'auto', '*', 'auto'],
+          headerRows: 1,
+          body: [
+            tableHead,
+            ...users.map((user, index) => [
+              { text: index + 1, alignment: 'center', margin: [0, 5] },
+              { text: user.firstName, alignment: 'center', margin: [0, 5] },
+              { text: user.lastName, alignment: 'center', margin: [0, 5] },
+              { text: user.email, alignment: 'center', margin: [0, 5] },
+              { text: user.phone, alignment: 'center', margin: [0, 5] },
+            ]),
+          ],
+        },
+      },
+    ],
+    styles: styles,
+  };
+};
