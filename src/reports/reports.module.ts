d8fcca5d0c9569dc6e35f2ee9e1f74787229f77c
franklin rodriguez <franklinserif@gmail.com>
2025-01-08 16:26:19 -0400

    feat(reports.module): import UsersModule, MembersModule, FieldsModule

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { PrinterService } from './printer.service';
+import { UsersModule } from '@users/users.module';
+import { MembersModule } from '@members/members.module';
+import { FieldsModule } from '@fields/fields.module';
 
 @Module({
   controllers: [ReportsController],
+  imports: [UsersModule, MembersModule, FieldsModule],
   providers: [ReportsService, PrinterService],
 })
 export class ReportsModule {}
