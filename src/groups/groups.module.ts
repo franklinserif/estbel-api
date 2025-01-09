a3546bb63f04c56c9ab548314d828b74636eb26b
franklin rodriguez <franklinserif@gmail.com>
2025-01-09 17:47:13 -0400

    feat(groups.module): add TypeOrmModule

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { GroupsController } from './groups.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Group } from './entities/group.entity';
 
 @Module({
   controllers: [GroupsController],
+  imports: [TypeOrmModule.forFeature([Group])],
   providers: [GroupsService],
+  exports: [TypeOrmModule],
 })
 export class GroupsModule {}
