94fa2516c89329c58f48dda281e98a260aacbfa3
franklin rodriguez <franklinserif@gmail.com>
2025-01-09 17:48:41 -0400

    feat(create-member.dto): add properties

--- src/members/dto/create-member.dto.ts
+++ src/members/dto/create-member.dto.ts
@@ -1,13 +1,76 @@
-import { CreateFieldValueDto } from '@fields/dto/create-field-value.dto';
-import { Type } from 'class-transformer';
-import { IsArray, IsString, ValidateNested } from 'class-validator';
+import {
+  IsString,
+  IsOptional,
+  IsEnum,
+  IsBoolean,
+  IsDateString,
+  IsUUID,
+  IsEmail,
+} from 'class-validator';
+import { Gender } from '@members/enum/options';
 
 export class CreateMemberDto {
-  @IsString()
-  ci: string;
+  @IsOptional()
+  @IsUUID()
+  id?: string;
 
-  @ValidateNested({ each: true })
-  @Type(() => CreateFieldValueDto)
-  @IsArray()
-  fields: CreateFieldValueDto[];
+  @IsOptional()
+  @IsString()
+  ci?: string;
+
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthdate?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  municipality?: string;
+
+  @IsOptional()
+  @IsString()
+  parish?: string;
+
+  @IsOptional()
+  @IsString()
+  zone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  howTheyArrived?: string;
+
+  @IsBoolean()
+  isBaptized: boolean;
+
+  @IsOptional()
+  @IsDateString()
+  baptizedAt?: Date;
+
+  @IsOptional()
+  @IsString()
+  baptizedChurch?: string;
+
+  @IsBoolean()
+  civilStatus: boolean;
+
+  @IsOptional()
+  @IsDateString()
+  weddingAt?: Date;
+
+  @IsDateString()
+  firstVisitAt: Date;
 }
