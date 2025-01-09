9c5d64903d89a5d9e3b37300d2087a5c66e4cf86
franklin rodriguez <franklinserif@gmail.com>
2025-01-09 16:11:17 -0400

    feat(groups.entity): add fields

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,1 +1,27 @@
-export class Group {}
+import { Member } from '@members/entities/member.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('groups')
+export class Group {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  name: string;
+
+  @ManyToOne(() => Member, (member) => member.groups)
+  member: Member;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
