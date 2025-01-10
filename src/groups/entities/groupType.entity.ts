1583b308397399ccf7f2fa28b08c1431d3b1993b
franklin rodriguez <franklinserif@gmail.com>
2025-01-10 16:26:42 -0400

    feat(entities): add GroupType

---
+++ src/groups/entities/groupType.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Group } from './group.entity';
+
+@Entity('group_type')
+export class GroupType {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  name: string;
+
+  @OneToMany(() => Group, (groups) => groups.groupTypes, { eager: true })
+  groups: Group[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
