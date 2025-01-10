703551aeb2589f045cfe43c1873a2a08f5a3ef87
franklin rodriguez <franklinserif@gmail.com>
2025-01-10 16:32:12 -0400

    refactor(user.entity): remove unnecessary fields
    these fields are no longer need it, these fields are in the member.entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -14,18 +14,6 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('text')
-  firstName: string;
-
-  @Column('text', {})
-  lastName: string;
-
-  @Column('text', { unique: true })
-  email: string;
-
-  @Column('text')
-  phone: string;
-
   @Column('text', {
     select: false,
   })
