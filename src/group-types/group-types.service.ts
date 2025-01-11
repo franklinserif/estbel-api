bac2204414a9750dcc6eb9c8ea13c45386a0e763
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 17:42:33 -0400

    feat(group-types.service): :sparkles: implementing services methods logic

--- src/group-types/group-types.service.ts
+++ src/group-types/group-types.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateGroupTypesDto } from './dto/create-group-types.dto';
 import { UpdateGroupTypesDto } from './dto/update-group-types.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GroupType } from './entities/group-types.entity';
+import { Repository } from 'typeorm';
+import { IQueryParams } from '@common/interfaces/decorators';
 
 @Injectable()
 export class GroupTypesService {
-  create(createGroupTypesDto: CreateGroupTypesDto) {
-    return 'This action adds a new groupsType';
+  constructor(
+    @InjectRepository(GroupType)
+    private readonly groupTypesService: Repository<GroupType>,
+  ) {}
+
+  async create(createGroupTypesDto: CreateGroupTypesDto) {
+    const groupType = this.groupTypesService.create(createGroupTypesDto);
+
+    return await this.groupTypesService.save(groupType);
   }
 
-  findAll() {
-    return `This action returns all groupsType`;
+  async findAll(queryParams: IQueryParams) {
+    const groupTypes = await this.groupTypesService.find(queryParams);
+    return groupTypes;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} groupsType`;
+  async findOne(id: string) {
+    const groupType = await this.groupTypesService.findOne({ where: { id } });
+
+    if (!groupType?.id) {
+      throw new NotFoundException(`group type with id: ${id} not found`);
+    }
+
+    return groupType;
   }
 
-  update(id: number, updateGroupTypesDto: UpdateGroupTypesDto) {
-    return `This action updates a #${id} groupsType`;
+  async update(id: string, updateGroupTypesDto: UpdateGroupTypesDto) {
+    await this.findOne(id);
+
+    return await this.groupTypesService.update(id, updateGroupTypesDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} groupsType`;
+  async remove(id: string) {
+    await this.findOne(id);
+    return await this.groupTypesService.delete(id);
   }
 }
