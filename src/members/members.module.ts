971ccaef003a79847736fc78b7842a40c6632dec
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 15:47:49 -0400

    fix(members.module): :bug: remove memberStatus from members.module

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -3,11 +3,10 @@ import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
-import { MemberStatus } from './entities/memberStatus.entity';
 
 @Module({
   controllers: [MembersController],
-  imports: [TypeOrmModule.forFeature([Member, MemberStatus])],
+  imports: [TypeOrmModule.forFeature([Member])],
   providers: [MembersService],
   exports: [TypeOrmModule, MembersService],
 })
