55c93c1e788704e184b758fa4668dda442d51dbb
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 16:40:17 -0400

    refactor(create-member-status.dto): :recycle: add Length min 2 to name property

--- src/member-status/dto/create-member-status.dto.ts
+++ src/member-status/dto/create-member-status.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 export class CreateMemberStatusDto {
   @IsString()
+  @Length(2)
   name: string;
 }
