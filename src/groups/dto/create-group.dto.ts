4bf5fa1ba4fdfb6cef33e05c371f1ee783a08fd5
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 16:38:51 -0400

    feat(CreateGroupDto): :sparkles: add property for validate data

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -1,1 +1,12 @@
-export class CreateGroupDto {}
+import { IsOptional, IsString, Length } from 'class-validator';
+
+export class CreateGroupDto {
+  @IsString()
+  @Length(2)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2)
+  location: string;
+}
