1164ae0dd9576980a0c9ca2dd14c0fd53818635e
franklin rodriguez <franklinserif@gmail.com>
2025-01-13 17:26:55 -0400

    refactor(create-user.dto): :recycle: remove unnecessary properties, make password more secure and change MinLength for IsUUID

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,21 +2,14 @@ import { IsString, MinLength, IsUUID, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  @MinLength(5)
-  firstName: string;
+  @IsUUID()
+  id: string;
 
   @IsString()
-  @MinLength(2)
-  lastName: string;
-
-  @IsString()
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  @IsPhoneNumber()
-  phone: string;
-
-  @IsString()
+  @MinLength(6)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/, {
+    message:
+      'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character, and be at least 6 characters long.',
+  })
   password: string;
 }
