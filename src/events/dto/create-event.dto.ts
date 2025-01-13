3208a8a2ba09b1466ee7c40e4f051f8376dd27b9
franklin rodriguez <franklinserif@gmail.com>
2025-01-13 17:21:18 -0400

    refactor(create-event.dto): :recycle: add MinLength to every string property

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,13 +1,16 @@
-import { IsBoolean, IsDate, IsString } from 'class-validator';
+import { IsBoolean, IsDate, IsString, MinLength } from 'class-validator';
 
 export class CreateEventDto {
   @IsString()
+  @MinLength(2)
   name: string;
 
   @IsString()
+  @MinLength(2)
   address: string;
 
   @IsString()
+  @MinLength(2)
   location: string;
 
   @IsBoolean()
