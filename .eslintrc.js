850711ab644348119a0d8a7ceaecd39e3dc0b003
franklin rodriguez <franklinserif@gmail.com>
2025-01-14 18:14:54 -0400

    chore(eslint): :sparkles: add eslint plugin for check code security

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:security/recommended-legacy',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
