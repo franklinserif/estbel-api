0e5c16b930764df7ef2e7683c098d795a92da20b
franklin rodriguez <franklinserif@gmail.com>
2025-01-14 22:34:41 -0400

    refactor(groups.service): :recycle: improve create and update group implementation

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -5,16 +5,34 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Group } from './entities/group.entity';
 import { Repository } from 'typeorm';
 import { IQueryParams } from '@common/interfaces/decorators';
+import { GroupType } from '@groupTypes/entities/group-types.entity';
 
 @Injectable()
 export class GroupsService {
   constructor(
     @InjectRepository(Group)
     private readonly groupService: Repository<Group>,
+
+    @InjectRepository(GroupType)
+    private readonly groupTypeService: Repository<GroupType>,
   ) {}
 
   async create(createGroupDto: CreateGroupDto) {
-    const group = this.groupService.create(createGroupDto);
+    const { groupTypeId } = createGroupDto;
+
+    const groupType = await this.groupTypeService.findOne({
+      where: { id: groupTypeId },
+    });
+
+    if (!groupType?.id) {
+      throw new NotFoundException(`GroupType with id ${groupTypeId} not found`);
+    }
+
+    const group = this.groupService.create({
+      ...createGroupDto,
+      groupTypes: groupType,
+    });
+
     return await this.groupService.save(group);
   }
 
@@ -37,7 +55,23 @@ export class GroupsService {
   async update(id: string, updateGroupDto: UpdateGroupDto) {
     await this.findOne(id);
 
-    return await this.groupService.update(id, updateGroupDto);
+    const { groupTypeId, ...restUpdateGroupDto } = updateGroupDto;
+
+    if (groupTypeId) {
+      const groupType = await this.groupTypeService.findOne({
+        where: { id: groupTypeId },
+      });
+
+      if (!groupType?.id) {
+        throw new NotFoundException(
+          `GroupType with id ${groupTypeId} not found`,
+        );
+      }
+
+      restUpdateGroupDto.groupTypes = groupType;
+    }
+
+    return await this.groupService.update(id, restUpdateGroupDto);
   }
 
   async remove(id: string) {
