659ea70c64002346f2a13f2d520334be3c16e57a
franklin rodriguez <franklinserif@gmail.com>
2025-01-14 22:36:20 -0400

    feat(members.module): :sparkles: import MemberStatus entity

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -3,10 +3,11 @@ import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
+import { MemberStatus } from '@memberStatus/entities/member-status.entity';
 
 @Module({
   controllers: [MembersController],
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member, MemberStatus])],
   providers: [MembersService],
   exports: [TypeOrmModule, MembersService],
 })
