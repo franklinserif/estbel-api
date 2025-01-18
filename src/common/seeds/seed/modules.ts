f08cbb2a75b6dca583c318ad19a012325bc7d285
franklin rodriguez <franklinserif@gmail.com>
2025-01-18 13:16:43 -0400

    refactor(seeds/seed/modules.ts): :recycle: change values

--- src/common/seeds/seed/modules.ts
+++ src/common/seeds/seed/modules.ts
@@ -1,8 +1,7 @@
-export const modules = [
-  { name: 'users', description: 'this module is for users adminitrators' },
-  {
-    name: 'modules',
-    description: 'this module refer to the module adminitration',
-  },
-  { name: 'members', description: 'this module is for users adminitrators' },
+export const MODULES = [
+  { name: 'users', description: 'Lorem Ipsum is simply dummy text' },
+  { name: 'modules', description: 'Lorem Ipsum is simply dummy text' },
+  { name: 'members', description: 'Lorem Ipsum is simply dummy text' },
+  { name: 'events', description: 'Lorem Ipsum is simply dummy text' },
+  { name: 'groups', description: 'Lorem Ipsum is simply dummy text' },
 ];
