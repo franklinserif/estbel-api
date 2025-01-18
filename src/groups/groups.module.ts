4118baf08b7b748174f433efe0ced9040fb6d5b8
franklin rodriguez <franklinserif@gmail.com>
2025-01-18 17:55:09 -0400

    feat(groups.module): :sparkles: add Member entity

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -4,10 +4,11 @@ import { GroupsController } from './groups.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from './entities/group.entity';
 import { GroupType } from '@groupTypes/entities/group-types.entity';
+import { Member } from '@members/entities/member.entity';
 
 @Module({
   controllers: [GroupsController],
-  imports: [TypeOrmModule.forFeature([Group, GroupType])],
+  imports: [TypeOrmModule.forFeature([Group, GroupType, Member])],
   providers: [GroupsService],
   exports: [TypeOrmModule],
 })
