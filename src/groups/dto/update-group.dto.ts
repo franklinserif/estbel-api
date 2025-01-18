556ac498ad3bedc01285817062c6dfddfb386a6d
franklin rodriguez <franklinserif@gmail.com>
2025-01-18 17:55:43 -0400

    refactor(update-group.dto): :recycle: add membersIds property

--- src/groups/dto/update-group.dto.ts
+++ src/groups/dto/update-group.dto.ts
@@ -1,7 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGroupDto } from './create-group.dto';
 import { GroupType } from '@groupTypes/entities/group-types.entity';
+import { ArrayNotEmpty, IsArray, IsOptional } from 'class-validator';
+import { IsUUIDOrCI } from '@members/decorators/is-uuidor-ci.decorator';
 
 export class UpdateGroupDto extends PartialType(CreateGroupDto) {
   groupTypes?: GroupType;
+
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsUUIDOrCI({ each: true })
+  membersIds?: string[];
 }
