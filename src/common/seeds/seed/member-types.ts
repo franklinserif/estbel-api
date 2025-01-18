80557f39b6f74e32a2e0cc4f2cb19e6781639698
franklin rodriguez <franklinserif@gmail.com>
2025-01-18 14:34:19 -0400

    feat(seeds/seed): :sparkles: add MEMBER_STATUS

---
+++ src/common/seeds/seed/member-types.ts
@@ -0,0 +1,22 @@
+export const MEMBER_STATUS = [
+  {
+    name: 'baptized',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting',
+  },
+  {
+    name: 'converted',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting',
+  },
+  {
+    name: 'visitor',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting',
+  },
+  {
+    name: 'member',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting',
+  },
+];
