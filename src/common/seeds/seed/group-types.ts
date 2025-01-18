2980ca12db412eb0930b100b880adbc598e65f08
franklin rodriguez <franklinserif@gmail.com>
2025-01-18 13:17:17 -0400

    feat(seeds/seed): :sparkles: add new seed for group-types, groups, and members

---
+++ src/common/seeds/seed/group-types.ts
@@ -0,0 +1,14 @@
+export const GROUP_TYPES = [
+  {
+    name: 'Ministerios',
+    description: 'Ministerios de la iglesia',
+  },
+  {
+    name: 'Grupos Homogeoneos',
+    description: 'Grupos Homogeneos de la iglesia',
+  },
+  {
+    name: 'Departamentos',
+    description: 'Departamentos de la iglesia',
+  },
+];
---
+++ src/common/seeds/seed/groups.ts
@@ -0,0 +1,22 @@
+export const GROUPS = [
+  {
+    name: 'Ministerio de Edificación',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    location: 'Iglesia Estrella de Belén',
+  },
+  {
+    name: 'Ministerio de Servicio',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    location: 'Iglesia Estrella de Belén',
+  },
+  {
+    name: 'Ministerio de Proclamación',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    location: 'Iglesia Estrella de Belén',
+  },
+  {
+    name: 'Ministerio de Adoración',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    location: 'Iglesia Estrella de Belén',
+  },
+];
