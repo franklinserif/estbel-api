68df8730b2287e28b42b6eaaa663508a23a7450f
franklin rodriguez <franklinserif@gmail.com>
2025-01-22 16:07:20 -0400

    docs(modules.service): :memo: add comments for every method

--- src/modules/modules.service.ts
+++ src/modules/modules.service.ts
@@ -13,21 +13,39 @@ export class ModulesService {
     private readonly moduleRepository: Repository<Module>,
   ) {}
 
-  async create(createModuleDto: CreateModuleDto) {
+  /**
+   * Creates a new module and saves it in the database.
+   *
+   * @param {CreateModuleDto} createModuleDto - The data transfer object for creating a module.
+   * @returns {Promise<Module>} The created module.
+   */
+  async create(createModuleDto: CreateModuleDto): Promise<Module> {
     const module = this.moduleRepository.create(createModuleDto);
 
     return await this.moduleRepository.save(module);
   }
 
-  async findAll(queryParams: IQueryParams) {
+  /**
+   * Retrieves all modules that match the provided query parameters.
+   *
+   * @param {IQueryParams} queryParams - Query parameters for filtering and sorting modules.
+   * @returns {Promise<Module[]>} An array of modules.
+   */
+  async findAll(queryParams: IQueryParams): Promise<Module[]> {
     const module = await this.moduleRepository.find(queryParams);
 
     return module;
   }
 
-  async findOne(id: string) {
+  /**
+   * Retrieves a single module by its ID.
+   *
+   * @param {string} id - The ID of the module to retrieve.
+   * @throws {NotFoundException} If no module with the given ID is found.
+   * @returns {Promise<Module>} The retrieved module.
+   */
+  async findOne(id: string): Promise<Module> {
     const module = await this.moduleRepository.findOne({ where: { id } });
-
     if (!module?.id) {
       throw new NotFoundException(`Module with id: ${id} not found`);
     }
@@ -35,7 +53,15 @@ export class ModulesService {
     return module;
   }
 
-  async update(id: string, updateModuleDto: UpdateModuleDto) {
+  /**
+   * Updates an existing module by its ID.
+   *
+   * @param {string} id - The ID of the module to update.
+   * @param {UpdateModuleDto} updateModuleDto - The data transfer object for updating a module.
+   * @throws {NotFoundException} If no module with the given ID is found.
+   * @returns {Promise<Module>} The result of the update operation.
+   */
+  async update(id: string, updateModuleDto: UpdateModuleDto): Promise<Module> {
     await this.findOne(id);
 
     const updatedModule = await this.moduleRepository.update(
@@ -46,7 +72,14 @@ export class ModulesService {
     return updatedModule;
   }
 
-  async remove(id: string) {
+  /**
+   * Deletes a module by its ID.
+   *
+   * @param {string} id - The ID of the module to delete.
+   * @throws {NotFoundException} If no module with the given ID is found.
+   * @returns {Promise<DeleteResult>} A promise indicating the completion of the delete operation.
+   */
+  async remove(id: string): Promise<DeleteResult> {
     await this.findOne(id);
 
     return await this.moduleRepository.delete(id);
