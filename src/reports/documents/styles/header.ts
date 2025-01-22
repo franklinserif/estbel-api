923bd7dbe81f957c2ea5ff61f712019d479a0648
franklin rodriguez <franklinserif@gmail.com>
2025-01-22 16:00:25 -0400

    refactor(documents/styles): :recycle: create an separate function for document headers
    all the documents must have the same header

---
+++ src/reports/documents/styles/header.ts
@@ -0,0 +1,26 @@
+import { getFormatterDate } from '@common/libs/date';
+import { Content } from 'pdfmake/interfaces';
+
+/**
+ * Generates a header for a PDF document.
+ * The header includes the current page number, total page count, and the current date.
+ *
+ * @param {number} currentPage - The current page number.
+ * @param {number} pageCount - The total number of pages in the document.
+ * @returns {Content} - A PDFMake `Content` object representing the header.
+ */
+export function docHeader(currentPage: number, pageCount: number): Content {
+  return {
+    style: 'header', // Apply the 'header' style
+    columns: [
+      {
+        text: `Page ${currentPage} of ${pageCount}`, // Display current page and total pages
+        alignment: 'left', // Left-align the page number text
+      },
+      {
+        text: getFormatterDate(), // Display the current date (formatted)
+        alignment: 'right', // Right-align the date text
+      },
+    ],
+  };
+}
