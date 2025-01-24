616f64520cef15008cba4b0185ece55890164b63
franklin rodriguez <franklinserif@gmail.com>
2025-01-23 23:36:39 -0400

    feat(groupMembers.report): :sparkles: generate a document base on members of a group and selected fields

--- src/reports/documents/groupMembers.report.ts
+++ src/reports/documents/groupMembers.report.ts
@@ -8,45 +8,101 @@ import { fillColorTable } from './styles/fillColorTable';
 import { docHeader } from './styles/header';
 import { Group } from '@groups/entities/group.entity';
 
-/**
- * Predefined styles for the PDF document.
- *
- * @type {StyleDictionary}
- */
+const COLUMN_CONFIG: Record<
+  string,
+  {
+    header: string;
+    accessor: (member: any, index: number) => string | number;
+  }
+> = {
+  'N°': {
+    header: 'N°',
+    accessor: (_, index: number) => index + 1,
+  },
+  'First Name': {
+    header: 'Nombre',
+    accessor: (member) => member.firstName,
+  },
+  'Last Name': {
+    header: 'Apellido',
+    accessor: (member) => member.lastName,
+  },
+  Gender: {
+    header: 'Sexo',
+    accessor: (member) => member.gender,
+  },
+  Phone: {
+    header: 'Teléfono',
+    accessor: (member) => member.phone || '---',
+  },
+  Country: {
+    header: 'Páis',
+    accessor: (member) => member.country || '---',
+  },
+  City: {
+    header: 'Ciudad',
+    accessor: (member) => member.city || '---',
+  },
+  Sector: {
+    header: 'Municipio',
+    accessor: (member) => member.location || '---',
+  },
+  Zone: {
+    header: 'Estado',
+    accessor: (member) => member.zone || '---',
+  },
+  Address: {
+    header: 'Dirección',
+    accessor: (member) => member.address || '---',
+  },
+  'Baptism Date': {
+    header: 'Fec. Bautismo',
+    accessor: (member) => getFormatterDate(member.baptizedAt) || '---',
+  },
+  'Baptism Church': {
+    header: 'Iglesia',
+    accessor: (member) => member.baptizedChurch || '---',
+  },
+  'Civil Status': {
+    header: 'Estado Civil',
+    accessor: (member) => member.civilStatus || '---',
+  },
+  'Group Name': {
+    header: 'Nombre del Grupo',
+    accessor: (member) => member.groupName || '---',
+  },
+};
+
 const styles: StyleDictionary = {
   header: { margin: [40, 20], fontSize: 14, alignment: 'right' },
   table: { margin: [0, 10, 0, 0], alignment: 'center' },
 };
 
-/**
- * Table headers for the PDF document.
- *
- * @type {Array<TableCell>}
- */
-const tableHead: TableCell[] = [
-  { text: 'N°' },
-  { text: 'Name' },
-  { text: 'Last Name' },
-  { text: 'Gender' },
-  { text: 'Phone' },
-  { text: 'Country' },
-  { text: 'City' },
-  { text: 'Sector' },
-  { text: 'Zone' },
-  { text: 'Address' },
-  { text: 'Baptism Date' },
-  { text: 'Baptism Church' },
-  { text: 'Civil Status' },
-  { text: 'Nombre del Grupo' },
-];
+export const groupMembersDoc = (
+  group: Group,
+  rows: string[] = Object.keys(COLUMN_CONFIG),
+): TDocumentDefinitions => {
+  const filteredColumns = rows
+    // eslint-disable-next-line security/detect-object-injection
+    .filter((row) => COLUMN_CONFIG[row])
+    // eslint-disable-next-line security/detect-object-injection
+    .map((row) => COLUMN_CONFIG[row]);
 
-/**
- * Generates a PDF document definition for the group list of members.
- *
- * @param {Group} group - the group member list to include in the PDF.
- * @returns {TDocumentDefinitions} The PDF document definition.
- */
-export const groupMembersDoc = (group: Group): TDocumentDefinitions => {
+  const tableHead: TableCell[] = filteredColumns.map((col) => ({
+    text: col.header,
+    style: 'tableHeader',
+  }));
+
+  // Generar cuerpo de la tabla
+  const tableBody = group.members.map((member, index) =>
+    filteredColumns.map((col) => ({
+      text:
+        col.accessor({ ...member, groupName: group.name }, index)?.toString() ||
+        '---',
+      alignment: 'center',
+    })),
+  );
+
   return {
     pageOrientation: 'landscape',
     pageSize: 'LEGAL',
@@ -61,32 +117,7 @@ export const groupMembersDoc = (group: Group): TDocumentDefinitions => {
         table: {
           dontBreakRows: true,
           headerRows: 1,
-          body: [
-            tableHead,
-            ...group.members.map((member, index) => [
-              { text: `${index + 1}`, alignment: 'center' },
-              { text: member.firstName, alignment: 'center' },
-              { text: member.lastName, alignment: 'center' },
-              { text: member.gender, alignment: 'center' },
-              { text: member.phone || '---', alignment: 'center' },
-              { text: member.country || '---', alignment: 'center' },
-              { text: member.city || '---', alignment: 'center' },
-              { text: member.location || '---', alignment: 'center' },
-              { text: member.zone || '---', alignment: 'center' },
-              { text: member.address || '---', alignment: 'center' },
-              {
-                text: getFormatterDate(member.baptizedAt) || '---',
-                alignment: 'center',
-              },
-              {
-                text: member.baptizedChurch || '---',
-                alignment: 'center',
-              },
-              { text: member.civilStatus || '---', alignment: 'center' },
-
-              { text: group.name || '---', alignment: 'center' },
-            ]),
-          ],
+          body: [tableHead, ...tableBody],
         },
       },
     ],
