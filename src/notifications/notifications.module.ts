0ec312efb1b56c20cb3216be89861ef7a7ee8323
franklin rodriguez <franklinserif@gmail.com>
2025-01-29 18:28:49 -0400

    feat(notifications.module): :sparkles: implement firebase-admin config

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as admin from 'firebase-admin';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 
 @Module({
   controllers: [NotificationsController],
+  imports: [ConfigModule.forRoot()],
   providers: [NotificationsService],
 })
-export class NotificationsModule {}
+export class NotificationsModule {
+  constructor() {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+}
