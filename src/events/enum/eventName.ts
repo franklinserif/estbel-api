51c8211686ac3798853af9f90c575d236d629286
franklin rodriguez <franklinserif@gmail.com>
2025-01-29 16:37:51 -0400

    feat(enums): :sparkles: add EEvent

---
+++ src/events/enum/eventName.ts
@@ -0,0 +1,7 @@
+export enum EEvent {
+  EVENT_CREATED = 'event.created',
+  EVENT_UPDATED = 'event.updated',
+  EVENT_DELETED = 'event.deleted',
+  EVENT_START = 'event.start',
+  EVENT_END = 'envt.end',
+}
