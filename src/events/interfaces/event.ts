4256de92b56e6f875ca522fe9c21030e7a9dfe93
franklin rodriguez <franklinserif@gmail.com>
2025-01-29 16:40:07 -0400

    feat(interfaces/events): :sparkles: add new interfaces

--- src/events/interfaces/event.ts
+++ src/events/interfaces/event.ts
@@ -1,3 +1,9 @@
 import { EEvent } from '@events/enum/eventName';
+import { Event } from '@events/entities/event.entity';
 
 export type TEventState = EEvent.EVENT_START | EEvent.EVENT_END;
+
+export interface IEvents {
+  old: Event;
+  new: Event;
+}
