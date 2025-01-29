7037090a471a03fe73117c4adf1616d72f82571e
franklin rodriguez <franklinserif@gmail.com>
2025-01-29 12:48:17 -0400

    style(events.module): :art: improve imports order

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MembersModule } from '@members/members.module';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { Attendance } from './entities/attendance.entity';
-import { MembersModule } from '@members/members.module';
 import { ScheduleService } from './schedule.service';
 import { AttendancesService } from './attendances.service';
 
