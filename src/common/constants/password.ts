1dc8c9b063e05d4d4661af02511ce44875ca8c5d
franklin rodriguez <franklinserif@gmail.com>
2025-01-31 18:29:34 -0400

    feat(common/constants): :sparkles: add password characters

---
+++ src/common/constants/password.ts
@@ -0,0 +1,4 @@
+export const UPPER_CASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz';
+export const NUMBERS = '0123456789';
+export const SYMBOLS = '!@#$%^&*()_+-=[]{}|;:,.<>?';
