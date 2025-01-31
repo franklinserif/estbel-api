e26182bc0e4082805d09c8c7c21efab3e98f825e
franklin rodriguez <franklinserif@gmail.com>
2025-01-31 16:56:36 -0400

    refactor(notifications.service): :fire: remove code that will be implement it in the firabase.service

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,33 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
-import * as admin from 'firebase-admin';
-import { SendNotificationDto } from './dtos/send-notification.dto';
 
 @Injectable()
 export class NotificationsService {
   private readonly logger: Logger = new Logger(NotificationsService.name);
-
-  async sendNotification(
-    sendNotificationDto: SendNotificationDto,
-  ): Promise<string> {
-    const { token, title, body, icon } = sendNotificationDto;
-    try {
-      const response = await admin.messaging().send({
-        token,
-        webpush: {
-          notification: {
-            title,
-            body,
-            icon,
-          },
-        },
-      });
-
-      return response;
-    } catch (error) {
-      this.logger.error(
-        `something went wrong sending the notification to this device ${sendNotificationDto.token} error: ${error}`,
-      );
-      throw error;
-    }
-  }
 }
