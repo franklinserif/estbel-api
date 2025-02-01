432f9905692af61e17f533a427b90b1dfb40c2e6
franklin rodriguez <franklinserif@gmail.com>
2025-02-01 12:06:41 -0400

    feat(email/dtos): :sparkles: add generatedPassword.dto

---
+++ src/email/dtos/generatedPassword.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsString, IsUrl, Matches, MinLength } from 'class-validator';
+
+export class GeneratedPasswordDto {
+  @IsString()
+  firstName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'The password must have at least 6 characters' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+    {
+      message:
+        'The password must have at least one simbol, one uppercase, one lowercase and one number',
+    },
+  )
+  password: string;
+
+  @IsString()
+  @IsUrl()
+  resetPasswordLink: string;
+}
