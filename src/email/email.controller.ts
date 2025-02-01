8cb94d3c4525be21a60cfab7e22c62fcbb10f401
franklin rodriguez <franklinserif@gmail.com>
2025-02-01 13:54:13 -0400

    feat(email.controller): :recycle: test service with the controller

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -6,7 +6,23 @@ export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Get('send-email')
-  sendWelcomeEmail() {
-    this.emailService.sendWellcomeEmail('franklinserif@gmail.com', 'franklin');
+  async sendWelcomeEmail() {
+    /*    await this.emailService.sendEmailToNewAccount({
+      email: 'franklinserif@gmail.com',
+      password: '*Fr04126674413',
+      activationLink: 'https://www.google.com',
+      to: 'franklinserif@gmail.com',
+      firstName: 'franklin',
+    }); */
+
+    this.emailService.sendEmailToNewPassword({
+      email: 'franklinserif@gmail.com',
+      resetPasswordLink: 'https://www.google.com',
+      password: '*Fr04126674413',
+      to: 'franklinserif@gmail.com',
+      firstName: 'franklin',
+    });
+
+    return { success: true, message: 'email sent' };
   }
 }
