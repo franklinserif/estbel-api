b28b5ac2b8779e83127322a7bd063b81716db6e9
franklin rodriguez <franklinserif@gmail.com>
2025-02-02 18:09:15 -0400

    feat(admins.module): :sparkles: import MembersModule & AccessesModule
    it's necessary for Member & Accesses entities

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -3,11 +3,18 @@ import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
 import { Admin } from './entities/admin.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MembersModule } from '@members/members.module';
+import { AccessesModule } from '@accesses/accesses.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   controllers: [AdminsController],
-  imports: [TypeOrmModule.forFeature([Admin])],
+  imports: [
+    TypeOrmModule.forFeature([Admin]),
+    EventEmitterModule.forRoot(),
+    MembersModule,
+    AccessesModule,
+  ],
   providers: [AdminsService],
   exports: [TypeOrmModule, AdminsService],
 })
