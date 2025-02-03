b783e5c296359473c2081e83d2996d3ccbeb2b4c
franklin rodriguez <franklinserif@gmail.com>
2025-02-03 14:56:37 -0400

    refactor(create-admin.to): :fire: remove password

--- src/admins/dto/create-admin.dto.ts
+++ src/admins/dto/create-admin.dto.ts
@@ -1,15 +1,7 @@
-import { IsString, MinLength, IsUUID, Matches } from 'class-validator';
+import { IsString, IsUUID } from 'class-validator';
 
 export class CreateAdminDto {
   @IsString()
   @IsUUID()
   id: string;
-
-  @IsString()
-  @MinLength(6)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/, {
-    message:
-      'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character, and be at least 6 characters long.',
-  })
-  password: string;
 }
