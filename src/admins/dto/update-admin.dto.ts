3117390ff34713570dbb43450d22ff32f9dd71e7
franklin rodriguez <franklinserif@gmail.com>
2025-02-03 14:57:02 -0400

    feat(update-admin.dto): :recycle: add password property

--- src/admins/dto/update-admin.dto.ts
+++ src/admins/dto/update-admin.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdminDto } from './create-admin.dto';
+import { IsString, Matches, MinLength } from 'class-validator';
 
-export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
+export class UpdateAdminDto extends PartialType(CreateAdminDto) {
+  @IsString()
+  @MinLength(6)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/, {
+    message:
+      'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character, and be at least 6 characters long.',
+  })
+  password: string;
+}
