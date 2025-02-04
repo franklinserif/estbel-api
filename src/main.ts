1ece6f3b1f3f02852c0eb31c2e7ff38beb8a6d41
franklin rodriguez <franklinserif@gmail.com>
2025-02-04 16:22:38 -0400

    refactor(main): :recycle: change process.env for configService

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from '@configEnv/enum/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number | undefined>(EnvironmentVariables.PORT);
+
+  await app.listen(port ?? 3000);
 }
 bootstrap();
