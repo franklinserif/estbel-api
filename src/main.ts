160a29dd108f69d2533050bda90817f647037b4f
franklin rodriguez <franklinserif@gmail.com>
2025-02-04 16:34:30 -0400

    feat(main): :sparkles: add global filter PostgressErrorFilter

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from '@configEnv/enum/env';
+import { PostgresErrorFilter } from '@common/errorsFilters/postgressErrorFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new PostgresErrorFilter());
 
   const configService = app.get(ConfigService);
   const port = configService.get<number | undefined>(EnvironmentVariables.PORT);
-
   await app.listen(port ?? 3000);
 }
 bootstrap();
