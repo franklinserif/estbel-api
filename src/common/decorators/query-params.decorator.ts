d87c28faf653a2fd3101846cfa64a489280f898d
franklin rodriguez <franklinserif@gmail.com>
2025-02-04 17:23:07 -0400

    refactor(query-params.decorator): :recycle: major update

--- src/common/decorators/query-params.decorator.ts
+++ src/common/decorators/query-params.decorator.ts
@@ -1,8 +1,25 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { Like, MoreThan, LessThan } from 'typeorm';
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+/* eslint-disable security/detect-object-injection */
+import { isEntity } from '@common/libs/isTypeOrmEntity';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  BadRequestException,
+} from '@nestjs/common';
+import { Like, MoreThan, LessThan, getMetadataArgsStorage } from 'typeorm';
 
 export const QueryParams = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (entity: any, ctx: ExecutionContext) => {
+    if (!isEntity(entity)) {
+      throw new BadRequestException(
+        'class must be an typeorm entity for validate QueryParams',
+      );
+    }
+
+    const entityFields = getMetadataArgsStorage()
+      .columns.filter((col) => col.target === entity)
+      .map((col) => col.propertyName);
+
     const request = ctx.switchToHttp().getRequest();
     const query = request.query;
 
@@ -12,24 +29,29 @@ export const QueryParams = createParamDecorator(
     Object.keys(query).forEach((key) => {
       const value = query[key];
 
+      const fieldName = key.replace(/(_like|_gt|_lt)$/, '');
+
+      if (!entityFields.includes(fieldName)) {
+        throw new BadRequestException(
+          `The field '${fieldName}' doesn't exist on entity`,
+        );
+      }
+
       if (key === 'orderBy') {
         const [column, direction] = value.split(':');
         if (['ASC', 'DESC'].includes(direction.toUpperCase())) {
           order[column] = direction.toUpperCase() as 'ASC' | 'DESC';
         }
       } else if (key.endsWith('_like')) {
-        const column = key.replace('_like', '');
-        where[column] = Like(`%${value}%`);
+        where[fieldName] = Like(`%${value}%`);
       } else if (key.endsWith('_gt')) {
-        const column = key.replace('_gt', '');
-        where[column] = MoreThan(value);
+        where[fieldName] = MoreThan(value);
       } else if (key.endsWith('_lt')) {
-        const column = key.replace('_lt', '');
-        where[column] = LessThan(value);
+        where[fieldName] = LessThan(value);
       } else if (value === 'true' || value === 'false') {
-        where[key] = value === 'true';
+        where[fieldName] = value === 'true';
       } else {
-        where[key] = value;
+        where[fieldName] = value;
       }
     });
 
