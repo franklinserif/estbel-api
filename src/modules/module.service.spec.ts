15408a037b88c99965477a582a39341779e42016
franklin rodriguez <franklinserif@gmail.com>
2025-02-04 21:47:40 -0400

    test(module.service.spec): :white_check_mark: add pass test

--- src/modules/module.service.spec.ts
+++ src/modules/module.service.spec.ts
@@ -1,16 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { v4 as uui } from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { ModulesService } from './modules.service';
 import { Module } from './entities/module.entity';
 import { CreateModuleDto } from './dto/create-module.dto';
+import { IQueryParams } from '@common/interfaces/decorators';
+import { NotFoundException } from '@nestjs/common';
+import { UpdateModuleDto } from './dto/update-module.dto';
+import { DeleteResult } from 'typeorm';
 
 export const MODULES = [
-  { name: 'users', description: 'Lorem Ipsum is simply dummy text' },
-  { name: 'modules', description: 'Lorem Ipsum is simply dummy text' },
-  { name: 'members', description: 'Lorem Ipsum is simply dummy text' },
-  { name: 'events', description: 'Lorem Ipsum is simply dummy text' },
-  { name: 'groups', description: 'Lorem Ipsum is simply dummy text' },
+  {
+    name: 'users',
+    description: 'Lorem Ipsum is simply dummy text',
+    id: uuid(),
+    createdAt: Date.now().toString(),
+    updatedAt: Date.now().toString(),
+  },
+  {
+    name: 'modules',
+    description: 'Lorem Ipsum is simply dummy text',
+    id: uuid(),
+    createdAt: Date.now().toString(),
+    updatedAt: Date.now().toString(),
+  },
+  {
+    name: 'members',
+    description: 'Lorem Ipsum is simply dummy text',
+    id: uuid(),
+    createdAt: Date.now().toString(),
+    updatedAt: Date.now().toString(),
+  },
+  {
+    name: 'events',
+    description: 'Lorem Ipsum is simply dummy text',
+    id: uuid(),
+    createdAt: Date.now().toString(),
+    updatedAt: Date.now().toString(),
+  },
+  {
+    name: 'groups',
+    description: 'Lorem Ipsum is simply dummy text',
+    id: uuid(),
+    createdAt: Date.now().toString(),
+    updatedAt: Date.now().toString(),
+  },
 ];
 
 describe('ModulesService', () => {
@@ -23,12 +57,44 @@ describe('ModulesService', () => {
 
     save: jest.fn().mockImplementation((dto: CreateModuleDto) =>
       Promise.resolve({
-        id: uui(),
+        id: uuid(),
         ...dto,
         createdAt: Date.now().toString(),
         updatedAt: Date.now().toString(),
       }),
     ),
+
+    find: jest.fn().mockImplementation((queryParams: IQueryParams) =>
+      Promise.resolve(
+        [...MODULES].filter((module) => {
+          if (queryParams?.where?.name) {
+            return module.name === queryParams.where?.name;
+          }
+
+          return true;
+        }),
+      ),
+    ),
+
+    findOne: jest.fn().mockImplementation(({ where: { id } }) => {
+      const moduleFound = MODULES.find((module) => module.id === id);
+      if (!moduleFound) {
+        throw new NotFoundException(`Module with id: ${id} not found`);
+      }
+      return Promise.resolve(moduleFound);
+    }),
+
+    update: jest.fn().mockImplementation((id: string, dto: UpdateModuleDto) =>
+      Promise.resolve({
+        ...MODULES.find((module) => module.id === id),
+        ...dto,
+        updatedAt: Date.now().toString(),
+      }),
+    ),
+
+    delete: jest.fn().mockImplementation(() => {
+      return Promise.resolve({ raw: [], affected: 1 } as DeleteResult);
+    }),
   };
 
   beforeEach(async () => {
@@ -45,19 +111,95 @@ describe('ModulesService', () => {
     modulesService = await moduleRef.resolve(ModulesService);
   });
 
-  it('should return a list of modules', async () => {});
+  it('should return a list of modules', async () => {
+    const spyFindAll = jest.spyOn(modulesService, 'findAll');
+    const modules = await modulesService.findAll({ where: {}, order: {} });
 
-  it('should create a module', async () => {
-    const spyCreate = jest.spyOn(modulesService, 'create');
-    const instantiatedModule = await modulesService.create(MODULES[0]);
+    expect(modules).toHaveLength(5);
+    expect(spyFindAll).toHaveBeenCalled();
+  });
 
-    expect(spyCreate).toHaveBeenCalled();
-    expect(spyCreate).toHaveBeenCalledWith(MODULES[0]);
-    expect(instantiatedModule).toEqual({
+  it('should search with queryParams an specific module', async () => {
+    const spyfindAll = jest.spyOn(modulesService, 'findAll');
+    const modules = await modulesService.findAll({
+      where: { name: 'users' },
+      order: {},
+    });
+
+    expect(spyfindAll).toHaveBeenCalled();
+    expect(modules).toHaveLength(1);
+    expect(modules[0]).toEqual({
       id: expect.any(String),
       ...MODULES[0],
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
+  });
+
+  it('should create a module', async () => {
+    const spyCreate = jest.spyOn(modulesService, 'create');
+    const createdModule = await modulesService.create({
+      name: MODULES[0].name,
+      description: MODULES[0].description,
+    });
+
+    expect(spyCreate).toHaveBeenCalled();
+    expect(spyCreate).toHaveBeenCalledWith({
+      name: MODULES[0].name,
+      description: MODULES[0].description,
+    });
+
+    expect(createdModule).toEqual({
+      id: expect.any(String),
+      name: MODULES[0].name,
+      description: MODULES[0].description,
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+  });
+
+  it('should find a module', async () => {
+    const spyOnFindOne = jest.spyOn(modulesService, 'findOne');
+    const moduleFound = await modulesService.findOne(MODULES[0].id);
+
+    expect(spyOnFindOne).toHaveBeenCalled();
+    expect(spyOnFindOne).toHaveBeenCalledWith(MODULES[0].id);
+    expect(moduleFound).toEqual(MODULES[0]);
+
+    const invalidId = '23423423xqwd';
+    await expect(modulesService.findOne(invalidId)).rejects.toThrow(
+      new NotFoundException(`Module with id: ${invalidId} not found`),
+    );
+  });
+
+  it('should update a module', async () => {
+    const spyOnUpdate = jest.spyOn(modulesService, 'update');
+    const updatedModule = await modulesService.update(MODULES[0].id, {
+      name: 'users module',
+    });
+
+    expect(spyOnUpdate).toHaveBeenCalled();
+    expect(spyOnUpdate).toHaveBeenCalledWith(MODULES[0].id, {
+      name: 'users module',
+    });
+    expect(updatedModule).toEqual({
+      ...MODULES[0],
+      updatedAt: expect.any(String),
+    });
+
+    const invalidId = '23423423xqwd';
+    await expect(
+      modulesService.update(invalidId, { name: 'invalid module' }),
+    ).rejects.toThrow(
+      new NotFoundException(`Module with id: ${invalidId} not found`),
+    );
+  });
+
+  it('should return a deleteResult', async () => {
+    const spyOnDelete = jest.spyOn(modulesService, 'remove');
+    const deleteResult = await modulesService.remove(MODULES[0].id);
+
+    expect(spyOnDelete).toHaveBeenCalled();
+    expect(deleteResult).toEqual({ raw: [], affected: 1 });
   });
 });
