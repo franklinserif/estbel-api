a4d75513c544de7d86f960d34d044032cff8ee6d
franklin rodriguez <franklinserif@gmail.com>
2025-02-19 21:40:07 -0400

    feat(auth.module): :sparkles: add jwt module configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from '@auth/auth.service';
 import { AuthController } from '@auth/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ENV_VAR } from '@configEnv/enum/env';
+import { AdminsModule } from '@admins/admins.module';
 
 @Module({
   controllers: [AuthController],
+  imports: [
+    AdminsModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>(ENV_VAR.JWT_SECRET),
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
