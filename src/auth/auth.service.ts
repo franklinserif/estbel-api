159570dc1765e79110237865d75d7d2dd8f57cd3
franklin rodriguez <franklinserif@gmail.com>
2025-02-20 18:14:57 -0400

    refactor(auth.service): :sparkles: add validateAdmin method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AdminsService } from '@admins/admins.service';
 import { comparePassword } from '@common/libs/password';
 import { Payload, Tokens } from './interfaces/jw';
+import { Admin } from '@admins/entities/admin.entity';
 
 @Injectable()
 export class AuthService {
@@ -28,5 +29,19 @@ export class AuthService {
     const refreshToken = this.jwtService.sign(payload, { expiresIn: '14d' });
 
     return { accessToken, refreshToken } as Tokens;
+  }
+
+  async validateAdmin(email: string, password: string): Promise<Admin | null> {
+    const admin = await this.admisnService.findByEmail(email);
+
+    const isValidPassword = await comparePassword(password, admin.password);
+
+    if (isValidPassword) {
+      delete admin.password;
+
+      return admin;
+    }
+
+    return null;
   }
 }
