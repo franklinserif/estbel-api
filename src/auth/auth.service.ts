1d01ac0dcea161510aa1a11537f665b6198afd99
franklin rodriguez <franklinserif@gmail.com>
2025-02-19 21:44:21 -0400

    feat(auth.service): :sparkles: add singIn method for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AdminsService } from '@admins/admins.service';
+import { comparePassword } from '@common/libs/password';
+import { Payload, Tokens } from './interfaces/jw';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private admisnService: AdminsService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(email: string, password: string): Promise<any> {
+    const admin = await this.admisnService.findByEmail(email);
+    const isValidPassword = await comparePassword(password, admin.password);
+
+    if (isValidPassword) {
+      throw new UnauthorizedException(`invalid password`);
+    }
+
+    const payload: Payload = {
+      sub: admin.id,
+      email: admin.email,
+    };
+
+    const accessToken = this.jwtService.sign(payload, { expiresIn: '5m' });
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: '14d' });
+
+    return { accessToken, refreshToken } as Tokens;
+  }
+}
