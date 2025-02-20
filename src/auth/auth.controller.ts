29ebe1525677965626c02b4dbe1e7141c0f19e0e
franklin rodriguez <franklinserif@gmail.com>
2025-02-19 21:45:43 -0400

    feat(auth.controller): :sparkles: add login post endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from '@auth/auth.service';
+import { LoginDto } from './dtos/login.dto';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { ENV_VAR } from '@configEnv/enum/env';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Post('login')
+  async signIn(@Body() loginDto: LoginDto, @Res() res: Response) {
+    const { accessToken, refreshToken } = await this.authService.signIn(
+      loginDto.email,
+      loginDto.password,
+    );
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get<string>(ENV_VAR.NODE_ENV) === 'prod',
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({ accessToken });
+  }
 }
