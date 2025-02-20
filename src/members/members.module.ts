77dd1c9ba244e9e53641ab8ae5f9ab5109c5cd4d
franklin rodriguez <franklinserif@gmail.com>
2025-02-20 18:13:45 -0400

    refactor(members.module): :sparkles: imports configuration module and add forwardRef to import AdminsModule

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MembersService } from '@members/members.service';
 import { MembersController } from '@members/members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from '@members/entities/member.entity';
 import { MemberStatusModule } from '@memberStatus/member-status.module';
+import { ConfigEnvModule } from '@configEnv/configEnv.module';
+import { AdminsModule } from '@admins/admins.module';
 
 @Module({
   controllers: [MembersController],
-  imports: [TypeOrmModule.forFeature([Member]), MemberStatusModule],
+  imports: [
+    forwardRef(() => AdminsModule),
+    TypeOrmModule.forFeature([Member]),
+    MemberStatusModule,
+    ConfigEnvModule,
+  ],
   providers: [MembersService],
   exports: [TypeOrmModule, MembersService],
 })
