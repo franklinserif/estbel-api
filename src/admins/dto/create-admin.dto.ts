09193614501654200532d9731c1a24b21ea1ce17
franklin rodriguez <franklinserif@gmail.com>
2025-02-20 18:11:09 -0400

    refactor(create-admin.dto): :recycle: change regex for validate password

--- src/admins/dto/create-admin.dto.ts
+++ src/admins/dto/create-admin.dto.ts
@@ -1,4 +1,5 @@
 import { IsUUIDOrCI } from '@members/decorators/is-uuidor-ci.decorator';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
 export class CreateAdminDto {
   @IsString()
@@ -7,7 +8,7 @@ export class CreateAdminDto {
 
   @IsString()
   @MinLength(6, { message: 'password must have at least 6 characters' })
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, {
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d).+$/, {
     message: 'Password must have at least 1 number and 1 character',
   })
   password: string;
