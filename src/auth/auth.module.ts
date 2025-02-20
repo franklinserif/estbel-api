b952995b817c4eb61fe691170afa70ba6184e67e
franklin rodriguez <franklinserif@gmail.com>
2025-02-20 18:13:00 -0400

    refactor(auth.module): :recycle: import configEnvModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from '@auth/auth.service';
 import { AuthController } from '@auth/auth.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ENV_VAR } from '@configEnv/enum/env';
 import { AdminsModule } from '@admins/admins.module';
+import { ConfigEnvModule } from '@configEnv/configEnv.module';
 
 @Module({
   controllers: [AuthController],
-  imports: [
-    AdminsModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>(ENV_VAR.JWT_SECRET),
-        signOptions: { expiresIn: '15m' },
-      }),
-    }),
-  ],
+  imports: [AdminsModule, ConfigEnvModule],
   providers: [AuthService],
 })
 export class AuthModule {}
