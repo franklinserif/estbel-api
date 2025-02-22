71ea2fd88ebab6c6558d25d56cc173fcb6892738
franklin rodriguez <franklinserif@gmail.com>
2025-02-21 22:41:59 -0400

    refactor(create-admin.dto): :fire: remove password
    it's not necessary anymore the password will autogenerated

--- src/admins/dto/create-admin.dto.ts
+++ src/admins/dto/create-admin.dto.ts
@@ -1,17 +1,10 @@
 import { IsUUIDOrCI } from '@members/decorators/is-uuidor-ci.decorator';
-import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateAdminDto {
   @IsString()
   @IsUUIDOrCI()
   id: string;
-
-  @IsString()
-  @MinLength(6, { message: 'password must have at least 6 characters' })
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d).+$/, {
-    message: 'Password must have at least 1 number and 1 character',
-  })
-  password: string;
 
   @IsEmail()
   email: string;
