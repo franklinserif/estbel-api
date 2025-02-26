5d6fb4545eb8b8a59d6bc7ab4f4128c5c616b3a2
franklin rodriguez <franklinserif@gmail.com>
2025-02-26 17:45:53 -0400

    chore(attendances.module): import MembersModule, EventsModule and Exports AttendancesService and TypeOrmModule

--- src/core/attendances/attendances.module.ts
+++ src/core/attendances/attendances.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AttendancesService } from './attendances.service';
-import { AttendancesController } from './attendances.controller';
+import { AttendancesService } from '@attendances/attendances.service';
+import { AttendancesController } from '@attendances/attendances.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Attendance } from '@attendances/entities/attendance.entity';
+import { MembersModule } from '@members/members.module';
+import { EventsModule } from '@events/events.module';
 
 @Module({
   controllers: [AttendancesController],
+  imports: [
+    TypeOrmModule.forFeature([Attendance]),
+    MembersModule,
+    EventsModule,
+  ],
   providers: [AttendancesService],
+  exports: [AttendancesService, TypeOrmModule],
 })
 export class AttendancesModule {}
