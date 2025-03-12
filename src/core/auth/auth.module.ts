e67b4855532625ce440dcff7d41c3e238c9871af
franklin rodriguez <franklinserif@gmail.com>
2025-03-12 10:44:10 -0400

    refactor(auth.module): add correct AdminModule importation by adding ForwardRef and exports AuthService

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from '@auth/auth.service';
 import { AuthController } from '@auth/auth.controller';
 import { AdminsModule } from '@admins/admins.module';
 import { ConfigurationModule } from '@configuration/configuration.module';
-
 @Module({
   controllers: [AuthController],
-  imports: [AdminsModule, ConfigurationModule],
+  imports: [ConfigurationModule, forwardRef(() => AdminsModule)],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
