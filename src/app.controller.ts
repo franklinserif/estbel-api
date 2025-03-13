6f0f532baf537be304095643e396fed374f1d781
franklin rodriguez <franklinserif@gmail.com>
2025-03-12 21:46:22 -0400

    feat(app.controller): update controller path to 'test' and add '/debug-sentry' endpoint for error simulation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -12,5 +12,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/debug-sentry')
+  getError() {
+    throw new Error('My first Sentry error!!!');
   }
 }
